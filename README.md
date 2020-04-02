29135b6b15604f0638dba231efd35e4783774323
Filip Ratkovic <ratkovic.filip@yahoo.com>
2020-04-02 19:40:41 -0400

    fr

---
+++ README.md
@@ -0,0 +1,23 @@
+# Updated Portfolio
+​
+#### Updated-Portfolio-Repo, 02/07/19
+#### Updated 03/31/20
+​
+#### By Filip Ratkovic
+​
+## Description
+​
+This is my professional developer portfolio with a simple but elegant style.
+​
+## Technologies Used
+​
+HTML
+​
+CSS, Bootstrap
+​
+Javascript, jquery
+​
+#  Direct link to site:
+https://filipratkovic.github.io/Updated-portfolio/
+
+
---
+++ assets/script/script.js
@@ -0,0 +1,51 @@
+$(".boredom-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://prpatel1231.github.io/BoredomApp/")
+});
+$("#boredom-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/prpatel1231/BoredomApp")
+});
+$(".spaRelax-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://project2-spa.herokuapp.com/")
+});
+$("#spaRelax-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/Monika933/Project-2-Spa")
+});
+
+$(".budgetTracker-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://offline-budget-application.herokuapp.com/")
+});
+$("#budgetTracker-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/filipratkovic/Progressive-Budget")
+});
+$(".weather-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://filipratkovic.github.io/Weather-Dashboard/")
+});
+$("#weather-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/filipratkovic/Weather-Dashboard")
+});
+
+$(".quiz-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://filipratkovic.github.io/quizgame/")
+});
+$("#quiz-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/filipratkovic/quizgame")
+});
+
+$(".crystal-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://filipratkovic.github.io/crystalgame/")
+});
+$("#crystal-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/filipratkovic/crystalgame")
+});
